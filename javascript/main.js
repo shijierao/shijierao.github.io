96c04f5f5e4e04267a233190279fc7b529a085bb
Shijie Rao <bobbyshijie@gmail.com>
2016-02-09 17:16:20 -0500

    currently under construction

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+// // jquery for detecting page scroll
+// $(function(){
+// 	var bodyHeight = $("body").height();
+// 	var windowHeight = $(window).height();
+// 	var footerHeight = $(".footer").height();
+// 	var contentHeight = $(".content").height();
+// 	var navheight = $(".top").height();
+// 	var logoHeight = $(".logo").height();
+// 	var barHeight = $(".nav").height();
+// 	console.log("bodyHeight: " + bodyHeight);
+// 	console.log("windowHeight: " + windowHeight);
+// 	console.log("footerHeight: " + footerHeight);
+// 	console.log("contentHeight: " + contentHeight);
+// 	console.log("navheight: " + navheight);
+// 	console.log("logoHeight: " + logoHeight);
+// 	console.log("barHeight: " + barHeight);
+// 	if (bodyHeight < windowHeight) {
+// 		console.log("scroll");
+		// $(".bottom").css("position", "absolute");
+// 	} else {
+		// $(".bottom").attr('id', 'stick');
+		// console.log("no");
+// 	}
+// });
