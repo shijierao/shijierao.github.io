e954d7ac42eec187a42ca0d088a12563b93d5b6c
Shijie Rao <bobbyshijie@gmail.com>
2016-02-12 05:02:31 -0500

    resume updated

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,7 @@
-// // jquery for detecting page scroll
-
-
-// $(function(){
-// 	var bodyHeight = $("body").height();
-// 	var windowHeight = $(window).height();
-// 	var footerHeight = $(".footer").height();
-// 	var contentHeight = $(".content").height();
-// 	var navheight = $(".top").height();
-// 	var logoHeight = $(".logo").height();
-// 	var barHeight = $(".nav").height();
-// 	console.log("bodyHeight: " + bodyHeight);
-// 	console.log("windowHeight: " + windowHeight);
-// 	console.log("footerHeight: " + footerHeight);
-// 	console.log("contentHeight: " + contentHeight);
-// 	console.log("navheight: " + navheight);
-// 	console.log("logoHeight: " + logoHeight);
-// 	console.log("barHeight: " + barHeight);
-// 	if (bodyHeight < windowHeight) {
-// 		console.log("scroll");
-		// $(".bottom").css("position", "absolute");
-// 	} else {
-		// $(".bottom").attr('id', 'stick');
-		// console.log("no");
-// 	}
-// });
+$(document).ready(function(){
+	var currentFile = document.location.pathname.match(/[^\/]+$/)[0].split(".")[0];
+	console.log(currentFile);
+	if(currentFile !== "index"){
+		$("#"+currentFile).css("font-weight","900");	
+	}
+});
