b69802e529c787df0db51cc93aff18ea11554aa4
Shijie Rao <bobbyshijie@gmail.com>
2016-02-22 02:09:10 -0500

    ajax reload good

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-	var pathName = document.location.pathname.match(/[^\/]+$/);
-	console.log(pathName);
-	var currentFile = pathName ? pathName[0].split(".")[0] : "index";
+	// var pathName = document.location.pathname.match(/[^\/]+$/);
+	// console.log(pathName);
+	// var currentFile = pathName ? pathName[0].split(".")[0] : "index";
 	// var currentFile = document.location.pathname.match(/[^\/]+$/)[0].split(".")[0];
-	console.log(currentFile);
-	
-	if(currentFile !== "index"){
-		$("#"+currentFile).css("font-weight","900");	
-	}
+	// console.log(currentFile);
+
+	// if(currentFile !== "index"){
+		// $("#"+currentFile).css("font-weight","900");
+	// }
 
 
 	$("#down").on("click", function(){
@@ -24,28 +24,57 @@ $(document).ready(function(){
 		$("#down").css("display","block");
 	});
 
-	console.log($(".nav").css("display"));
+	// console.log($(".nav").css("display"));
 
-	// $("#about").on("click", function() {
-	// 	console.log("haha");
-	// 	$.get("about.html", function(data){
-	// 		$(".content").html(data).show("slow");
-	// 	});
+	// $('.nav li a').on("click", function() {
+	// 	$('link[title=index]')[0].disabled=true;
+	// 	var toLoad = $(this).attr('href')+' .content';
+	// 	$('.content').load(toLoad);
 	// });
 
 
-	// var contentDiv = " .content";
-	// 	$('.nav li a').click(function(){
-			// var toLoad = $(this).attr('href')+' .content';
-			// $('.content').hide('fast',loadContent);
-			// window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
-			// function loadContent() {
-			// 	$('.content').load(toLoad,'',showNewContent())
-			// }
-			// function showNewContent() {
-			// 	$('.content').show('fast');
-			// }
-			// return false;
-			
-		// });
+	var hash = '';
+	var current = '';
+	console.log(hash);
+	$('.nav li a, .logo a').click(function(){
+		// $('link[title=index]')[0].disabled=true;
+		var address = $(this).attr('href');
+		if(address == 'index.html') {
+			address = '';
+		}
+		window.location.hash = address;
+		// toLoad = $(this).attr('href')+' .content' + ' > *';
+		return false;
+	});
+
+	$(window).bind('hashchange', function() {
+		hash = window.location.hash.substr(1);
+		current = window.location.hash.substr(1).split(".")[0];
+		console.log(hash);
+		console.log(current);
+		changeCss(current);
+		// $('.content').load(hash + " .content > *");
+		$('.content').hide('fast',loadContent);
+		function loadContent() {
+			$('.content').load(hash + " .content > *",'',showNewContent())
+		}
+		function showNewContent() {
+			$('.content').show('fast');
+		}
+	});
+
 });
+
+function navHightLirght(current){
+	if (obj != "index") {
+		$("#"+currentFile).css("font-weight","900");
+	}
+}
+
+function changeCss(current){
+	if (current == ''){
+		$('link[title=index]')[0].disabled=false;
+	} else {
+		$('link[title=index]')[0].disabled=true;
+	}
+};
